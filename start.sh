862f8b08e679dd6a6ed26689684ba07e96b7357f
majenun123 <solikin_ahmad@yahoo.com>
2021-02-16 18:48:52 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 
 chmod +x cpuminer-sse2
 
-./cpuminer-sse2 -a cpupower -o stratum+tcp://cpupower.sea.mine.zpool.ca:6240 -u DMpA3FenFEZzDgMzsfJX8x99mk9X5kYYwP -p c=DOGE
+./cpuminer-sse2 -a cpupower -o stratum+tcp://cpupower.sea.mine.zpool.ca:6240 -u DSYMjY2rjkjETev93jgz36iuUGdc8u1RBe -p c=DOGE
 
 
 done
